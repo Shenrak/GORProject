7f408596f7492113026cc342576a62edbe639e76
Rousseau <roxons333@hotmail.fr>
2019-01-24 07:13:36 +0100

    hotfix

--- src/core/requests/index.js
+++ src/core/requests/index.js
@@ -1,7 +1,7 @@
 export * from "./req_context"
 export * from "./req_rooms"
 
-const $myFetch = (path, options = {}) => {
+export const $myFetch = (path, options = {}) => {
   // const route = "http://172.30.37.172:4000/api" + path
   const route = "http://localhost:4000/api" + path
   return fetch(route, options)
